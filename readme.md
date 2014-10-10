124bf25257f740f6ee54f866c51a40b084ec610f
zhuzhichao <me@zhuzhichao.com>
2014-10-10 09:42:30 +0800

    数据库版本更新，修复bug，更新readme

--- readme.md
+++ readme.md
@@ -1,19 +1,19 @@
 #Ip Location Zh
 ----
 这是一个能够通过Ip，获取该IP所在的位置，例如通过ip：`1.192.94.203`可以获得是`中国河南郑州`,同样能获得国外的地址，但是没有国内详细。
-**注：基于[17mon.cn]([http://tool.17mon.cn/ipdb.html])的IP数据库进行制作，该数据库每2个月更新一次，届时同步更新该插件**
+**注：基于[17mon.cn](http://tool.17mon.cn/ipdb.html)的IP数据库进行制作，该数据库每2个月更新一次，届时同步更新该插件**
 
 ##特点
 ----
 1. 不配置和使用数据库，妈妈再也不用担心配置问题了
 2. 使用简单，功能专（dan）注（yi）
-3. 使用[composer]([https://getcomposer.org/])进行安装管理，国际标准，方便快捷，即安即用，随时更新数据库
+3. 使用[composer](https://getcomposer.org/)进行安装管理，国际标准，方便快捷，即安即用，随时更新数据库
 4. 部分数据参考了纯真 IP 库、淘宝 IP 库、腾讯 IP 库、新浪 IP 库、中国互联网广告行业 IP 库的数据，还包括 BGP.HE.NET 以及全球各大地区的 IP 管理机构的 WHOIS 信息数据
 
 ##Install && Use
 ----
 ###Install Composer
-这里不详细介绍安装composer了，大家根据[链接]([https://getcomposer.org/])自行安装吧！什么？没听过？你真的需要脑补了，赶快行动吧！^^
+这里不详细介绍安装composer了，大家根据[链接](https://getcomposer.org/)自行安装吧！什么？没听过？你真的需要脑补了，赶快行动吧！^^
 
 ###Use
 如果已经有了`composer.json`文件的话，直接添加`"zhuzhichao/ip-location-zh": "1.*"` 到依赖，然后执行`composer update`。
@@ -38,7 +38,7 @@
 对，很简单，只用一个方法，那就是`find`。
 
 ##Contributing
-有什么新的想法和建议，欢迎提交[issue]([https://github.com/zhuzhichao/ip-location-zh/issues])或者[Pull Requests]([https://github.com/zhuzhichao/ip-location-zh/pulls])。
+有什么新的想法和建议，欢迎提交[issue](https://github.com/zhuzhichao/ip-location-zh/issues)或者[Pull Requests](https://github.com/zhuzhichao/ip-location-zh/pulls)。
 
 ##License
 MIT
--- src/IpLocationZh.php
+++ src/IpLocationZh.php
@@ -1,8 +1,16 @@
-<?php
+<?php namespace Zhuzhichao\IpLocationZh;
 
-namespace Zhuzhichao\IpLocationZh;
+/*
+    全球 IPv4 地址归属地数据库(17MON.CN 版)
+    高春辉(pAUL gAO) <gaochunhui@gmail.com>
+    Build 20141009 版权所有 17MON.CN
+    (C) 2006 - 2014 保留所有权利
+    请注意及时更新 IP 数据库版本
+    数据问题请加 QQ 群: 346280296
+    Code for PHP 5.3+ only
+*/
 
-class Ip {
+class IP {
 	private static $ip = NULL;
 
 	private static $fp = NULL;
@@ -31,7 +39,7 @@ class Ip {
 			self::init();
 		}
 
-		$nip = pack('N', ip2long($nip));
+		$nip2 = pack('N', ip2long($nip));
 
 		$tmp_offset = (int)$ipdot[0] * 4;
 		$start      = unpack('Vlen', self::$index[$tmp_offset].self::$index[$tmp_offset + 1].self::$index[$tmp_offset + 2].self::$index[$tmp_offset + 3]);
@@ -39,7 +47,7 @@ class Ip {
 		$index_offset = $index_length = NULL;
 		$max_comp_len = self::$offset['len'] - 1028;
 		for ($start = $start['len'] * 8 + 1024; $start < $max_comp_len; $start += 8) {
-			if (self::$index{$start}.self::$index{$start + 1}.self::$index{$start + 2}.self::$index{$start + 3} >= $nip) {
+			if (self::$index{$start}.self::$index{$start + 1}.self::$index{$start + 2}.self::$index{$start + 3} >= $nip2) {
 				$index_offset = unpack('Vlen', self::$index{$start + 4}.self::$index{$start + 5}.self::$index{$start + 6}."\x0");
 				$index_length = unpack('Clen', self::$index{$start + 7});
 
@@ -60,7 +68,7 @@ class Ip {
 
 	private static function init() {
 		if (self::$fp === NULL) {
-			self::$ip = new IP();
+			self::$ip = new self();
 
 			self::$fp = fopen(__DIR__.'/17monipdb.dat', 'rb');
 			if (self::$fp === FALSE) {
