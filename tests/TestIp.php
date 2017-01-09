426079e6c78fc1c8029792fcc10e9f1d8c7b5b86
zhuzhichao <me@zhuzhichao.com>
2017-01-09 17:46:55 +0800

    更新：测试数据

--- tests/TestIp.php
+++ tests/TestIp.php
@@ -4,7 +4,7 @@ use Zhuzhichao\IpLocationZh\Ip;
 
 class TestIp extends PHPUnit_Framework_TestCase {
 	public function testIpLocation() {
-		$this->assertEquals(['中国', '河南', '郑州', ''], Ip::find('1.192.94.203'));
+		$this->assertEquals(['中国', '河南', '郑州', ''], Ip::find('171.12.10.156'));
 		$this->assertEquals(['中国', '浙江', '杭州', ''], Ip::find('110.75.115.70'));
 	}
 }
