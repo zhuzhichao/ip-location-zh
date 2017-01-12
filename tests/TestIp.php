0962b887985022f9a5c807836e9a938d7287eb4e
zhuzhichao <me@zhuzhichao.com>
2017-01-12 09:08:05 +0800

    添加本机地址的测试

--- tests/TestIp.php
+++ tests/TestIp.php
@@ -4,7 +4,8 @@ use Zhuzhichao\IpLocationZh\Ip;
 
 class TestIp extends PHPUnit_Framework_TestCase {
 	public function testIpLocation() {
-		$this->assertEquals(['中国', '河南', '郑州', '', '410100'], Ip::find('171.12.10.156'));
+        $this->assertEquals(['中国', '河南', '郑州', '', '410100'], Ip::find('171.12.10.156'));
+        $this->assertEquals(['本机地址', '本机地址', '', '', ''], Ip::find('127.0.0.1'));
 		$this->assertEquals(['中国', '浙江', '杭州', '', '330100'], Ip::find('110.75.115.70'));
 	}
 }
